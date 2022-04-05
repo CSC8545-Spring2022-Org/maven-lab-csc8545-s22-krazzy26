a6b3c740bf5d403dc0577524cda88463e2fa6af6
Anushree Prakash <anushree.apb@gmail.com>
2022-04-05 01:19:06 -0400

    Add Unirest Demo

---
+++ src/main/java/edu/studio/sample/GitHubUnirestDemo.java
@@ -0,0 +1,20 @@
+package edu.studio.sample;
+
+import kong.unirest.Unirest;
+
+public class GitHubUnirestDemo {
+    
+    public static final String GITHUB_API_URI = "https://api.github.com/repos/CSC8545-Spring2022-Org/maven-lab-csc8545-s22-krazzy26/issues";
+
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+        String getAllResponse = 
+                Unirest.get(GITHUB_API_URI)
+                .header("Authorization", "Bearer " + args[0])
+                .asString()
+                .getBody();
+        
+        System.out.println("GET ALL" + getAllResponse);
+    }
+
+}
